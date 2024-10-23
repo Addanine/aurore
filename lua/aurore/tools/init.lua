542fd997ca7e9b72ad5b8069dc23baf631c92372
harryoltmans <harry@adeninelabs.com>
2024-10-23 11:38:27 -0700

    test

--- lua/aurore/tools/init.lua
+++ lua/aurore/tools/init.lua
@@ -1,1 +1,18 @@
+-- init.lua
+local M = {}
 
+M.setup = function(opts)
+    -- Load default config
+    local config = require('aurore.config')
+    
+    -- Merge user options with defaults
+    config.setup(opts)
+    
+    -- Initialize API
+    require('aurore.api').setup()
+    
+    -- Initialize UI
+    require('aurore.ui').setup()
+end
+
+return M
