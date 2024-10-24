f49ce94ad46f4b8f2c4a1be452ac68640fdb1a8a
harryoltmans <harry@adeninelabs.com>
2024-10-23 23:38:22 -0700

    cleaned

--- lua/aurore/init.lua
+++ lua/aurore/init.lua
@@ -1,18 +1,14 @@
 local M = {}
 
 M.setup = function(opts)
-    -- Debug prints
-    vim.notify("Loading aurore with opts: " .. vim.inspect(opts))
+    -- Only show messages if quiet is not enabled
+    if not opts.quiet then
+        vim.notify("Loading aurore with opts: " .. vim.inspect(opts))
+    end
     
-    -- First load config
     local config = require('aurore.config')
-    vim.notify("Config loaded")
-    
-    -- Setup config
     config.setup(opts)
-    vim.notify("Config setup complete")
-
-    -- Initialize components
+    
     require('aurore.api').setup(config.options)
     require('aurore.ui').setup(config.options)
     require('aurore.tools').setup()
